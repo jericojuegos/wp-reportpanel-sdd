d8c5923056d7db1981e824c38e1dd03f244b4c98
jericojuegos <juegos.jerico@yahoo.com>
2025-10-15 15:31:50 +0800

    refactor: restructure plugin into a class-based architecture and update settings registration

--- includes/admin/settings.php
+++ includes/admin/settings.php
@@ -1,74 +1,91 @@
-<?php
+<?php declare(strict_types=1);
+
+namespace Jay\WpReportPanelSdd\Admin;
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
 use tangible\framework;
+use Jay\WpReportPanelSdd\Plugin;
 
-framework\register_plugin_settings($plugin, [
-  'css' => $plugin->assets_url . '/build/admin.min.css',
-  'title_callback' => function() use ($plugin) {
-    ?>
-      <img class="plugin-logo"
-        src="<?= $plugin->assets_url ?>/images/tangible-logo.png"
-        alt="Test Logo"
-        width="40"
-      >
-      <?= $plugin->title ?>
-    <?php
-  },
-  'tabs' => [
-    'welcome' => [
-      'title' => 'Welcome',
-      'callback' => function() {
-        ?>Hello, world.<?php
-      }
-    ],
-    'features' => [
-      'title' => 'Features',
-      'callback' => function() use ($plugin) {
-        framework\render_features_settings_page($plugin);
-      }
-    ],
-  ],
+class Settings {
+    
+    public function __construct() {
+      $this->register_settings();
+    }
+    
+    public function register_settings() {
+      \tangible\framework\register_plugin_settings(Plugin::$plugin, [
+        'css' => Plugin::$plugin->assets_url . '/build/admin.min.css',
+        'title_callback' => function() {
+            ?>
+            <img class="plugin-logo"
+                src="<?= Plugin::$plugin->assets_url ?>/images/tangible-logo.png"
+                alt="Test Logo"
+                width="40"
+            >
+            <?= Plugin::$plugin->title ?>
+            <?php
+        },
+        'tabs' => [
+            'welcome' => [
+                'title' => 'Welcome',
+                'callback' => function() {
+                  ?>
+                    Hello, world.
 
-  'features' => [
-    [
-      'name' => 'example',
-      'title' => 'First feature',
-      'entry_file' => __DIR__ . '/../features/example.php'
-    ],
-    [
-      'name' => 'example_2',
-      'title' => 'Second feature',
-      'entry_file' => __DIR__ . '/../features/example-2.php',
-      // 'default' => true,
-    ],
-  ],
+                    <ul>
+                      <li>Plugin: <?php echo Plugin::$plugin->title; ?></li>
+                      <li>Version: <?php echo Plugin::$plugin->version; ?></li>
+                      <li>Assets URL: <?php echo Plugin::$plugin->assets_url; ?></li>                  
+                    </ul>
 
-]);
+                  <?php
+                }
+            ],
+            'features' => [
+                'title' => 'Features',
+                'callback' => function() {
+                    framework\render_features_settings_page( Plugin::$plugin );
+                }
+            ],
+        ],
+        'features' => [
+            [
+                'name' => 'example',
+                'title' => 'First feature',
+                'entry_file' => __DIR__ . '/../features/example.php'
+            ],
+            [
+                'name' => 'example_2',
+                'title' => 'Second feature',
+                'entry_file' => __DIR__ . '/../features/example-2.php',
+            ],
+        ],
+      ]);
+    }
+    
+    public function register_admin_notice() {
+        $welcome_notice_key = Plugin::$plugin->setting_prefix . '_welcome_notice';
 
-framework\register_admin_notice(function() use ($plugin) {
+        if (framework\is_admin_notice_dismissed($welcome_notice_key)) {
+            return;
+        }
 
-  $welcome_notice_key = $plugin->setting_prefix . '_welcome_notice';
+        if (isset($_GET['dismiss_admin_notice'])) {
+            framework\dismiss_admin_notice( $welcome_notice_key );
+            return;
+        }
 
-  if ( framework\is_admin_notice_dismissed( $welcome_notice_key ) ) {
-    return;
-  }
-
-  if (isset($_GET['dismiss_admin_notice'])) {
-    // Remove notice after clicking link to welcome page
-    framework\dismiss_admin_notice( $welcome_notice_key );
-    return;
-  }
-
-  ?>
-  <div class="notice notice-info is-dismissible"
-    data-tangible-admin-notice="<?php echo $welcome_notice_key; ?>"
-  >
-    <p>Welcome to <b><?php echo $plugin->title; ?></b>. Please see the <a href="<?php
-      $url = (is_multisite() ? 'settings.php' : 'options-general.php')
-        . "?page={$plugin->name}-settings&dismiss_admin_notice=true"
-      ;
-      echo esc_attr($url);
-    ?>">plugin settings page</a> to get started.</p>
-  </div>
-  <?php
-
-});
+        ?>
+        <div class="notice notice-info is-dismissible"
+            data-tangible-admin-notice="<?php echo $welcome_notice_key; ?>"
+        >
+            <p>Welcome to <b><?php echo Plugin::$plugin->title; ?></b>. Please see the <a href="<?php
+                $url = (is_multisite() ? 'settings.php' : 'options-general.php') . "?page=" . Plugin::$plugin->name . "-settings&dismiss_admin_notice=true";
+                echo esc_attr($url);
+            ?>">plugin settings page</a> to get started.</p>
+        </div>
+        <?php
+    }
+    
+}
--- wp-reportpanel-sdd.php
+++ wp-reportpanel-sdd.php
@@ -1,13 +1,16 @@
 <?php
 /**
  * Plugin Name: Report Panel
- * Plugin URI: https://tangibleplugins.com/wp-reportpanel-sdd
  * Description: 
  * Version: 0.1.1
- * Author: Team Tangible
- * Author URI: https://teamtangible.com
+ * Author: Jerico Juegos
  * License: GPLv2 or later
  */
+
+namespace Jay\WpReportpanelSdd;
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Prevent direct access
+
 use tangible\framework;
 use tangible\updater;
 
@@ -16,38 +19,46 @@ define( 'WP_REPORTPANEL_SDD_VERSION', '0.1.1' );
 require __DIR__ . '/vendor/tangible/framework/index.php';
 require __DIR__ . '/vendor/tangible/updater/index.php';
 
-/**
- * Get plugin instance
- */
-function wp_reportpanel_sdd($instance = false) {
-  static $plugin;
-  return $plugin ? $plugin : ($plugin = $instance);
+class Plugin {
+    public static $plugin;
+    public $settings;
+
+    public function __construct() {
+        add_action( 'plugins_loaded', [ $this, 'init_framework' ] );
+        add_action( 'plugins_loaded', [ $this, 'init_updater' ], 11 );
+        add_action( 'plugins_loaded', [ $this, 'load_includes' ], 12 );
+    }
+ 
+    public function init_framework() {
+        if ( defined( 'WP_SANDBOX_SCRAPING' ) ) return;
+
+        self::$plugin = framework\register_plugin([
+            'name'           => 'wp-reportpanel-sdd',
+            'title'          => 'Report Panel',
+            'setting_prefix' => 'wp_reportpanel_sdd',
+            'version'        => '0.1.1',
+            'file_path'      => __FILE__,
+            'base_path'      => plugin_basename( __FILE__ ),
+            'dir_path'       => plugin_dir_path( __FILE__ ),
+            'url'            => plugins_url( '/', __FILE__ ),
+            'assets_url'     => plugins_url( '/assets', __FILE__ ),
+        ]);
+    }
+
+    public function init_updater() {
+        updater\register_plugin([
+            'name' => self::$plugin->name ?? 'wp-reportpanel-sdd',
+            'file' => __FILE__,
+        ]);
+    }
+
+    public function load_includes() {
+        require_once __DIR__ . '/includes/admin/settings.php';
+
+        $this->settings = new \Jay\WpReportpanelSdd\Admin\Settings( self::$plugin );
+    }
+
 }
 
-add_action('plugins_loaded', function() {
-
-  // See https://github.com/TangibleInc/framework/#note-on-plugin-activation
-  if (defined('WP_SANDBOX_SCRAPING')) return;
-
-  $plugin    = framework\register_plugin([
-    'name'           => 'wp-reportpanel-sdd',
-    'title'          => 'Report Panel',
-    'setting_prefix' => 'wp_reportpanel_sdd',
-    'version'        => WP_REPORTPANEL_SDD_VERSION,
-    'file_path' => __FILE__,
-    'base_path' => plugin_basename( __FILE__ ),
-    'dir_path' => plugin_dir_path( __FILE__ ),
-    'url' => plugins_url( '/', __FILE__ ),
-    'assets_url' => plugins_url( '/assets', __FILE__ ),
-  ]);
-
-  updater\register_plugin([
-    'name' => $plugin->name,
-    'file' => __FILE__,    
-  ]);
-
-  wp_reportpanel_sdd( $plugin );
-
-  require_once __DIR__.'/includes/index.php';
-
-}, 10);
+// Initialize the plugin
+new Plugin();
